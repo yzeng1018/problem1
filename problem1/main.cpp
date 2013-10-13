976cff76345b0f4d1155470cc45e16469886ea69
yan zeng <yanzeng@yan-zengs-MacBook-Pro.local>
2013-10-13 14:08:30 -0400

    Initial Commit

---
+++ problem1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  problem1
+//
+//  Created by yan zeng on 10/13/13.
+//  Copyright (c) 2013 yan zeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

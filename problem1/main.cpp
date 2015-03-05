295fe054c81ba1315a806bf4a1cf090d2d651f1c
yan <yzeng1018@gmail.com>
2015-03-05 23:24:46 +0800

    Revert "try just"
    This reverts commit 66ff421e7394b646040039556bcdcfc0c5cd7d0e.

--- problem1/main.cpp
+++ problem1/main.cpp
@@ -7,31 +7,12 @@
 //
 
 #include <iostream>
-using namespace std;
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    int sum = 0;
-    int A[99];
-    for (int i = 0 ; i < 99; i++) {
-        cout << "please record the next solider's parch number:" << endl;
-        cin >> A[i];
-        if (A[i] > 100 || A[i] < 1) {
-            cout << " please enter an interger between 1 to 100:" << endl;
-            cin >> A[i];
-        }
-        for (int j = 0; j < i; j++) {
-            if (A[j] ==A[i]) {
-                cout << " you can't enter an existing number, please enter a new number:" << endl;
-                cin >> A[i];
-            }
-        }
-        sum = sum + A[i];
-        
-    }
-    cout << "the Spy's parch number is " <<5050-sum ;
+    std::cout << "Hello, World!\n";
     return 0;
 }
 
